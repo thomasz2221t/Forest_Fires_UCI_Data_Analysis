cd2060be60d778f79770786089f6165a5caaac22
adamwar7 <56248521+adamwar7@users.noreply.github.com>
2021-03-20 12:14:03 +0100

    Funkcje

---
+++ src/UsefullFunctions/install-all-needed-packages.R
@@ -0,0 +1,6 @@
+installAllNeededPackages<- function(){
+  install.packages("ggplot2")
+  install.packages("Amelia")
+  install.packages("mlbench")
+  install.packages("tidyverse")
+}
---
+++ src/UsefullFunctions/read-csv-and-recode.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+ReadCSVAndRecodeMonthsAndDays <- function()
+{
+  foresty <- read.csv(file = 'forestfires.csv')
+  foresty$month <- recode(foresty$month, "jan"=1, "feb"=2, "mar"=2, "apr"=4,"may"=5, "jun"=6, "jul"=7, "aug"=8,"sep"=9, "oct"=10, "nov"=11, "dec"=12)
+  foresty$day <- recode(foresty$day, "mon"=1, "tue"=2, "wed"=2, "thu"=4,"fri"=5, "sat"=6, "sun"=7)
+  return(foresty)
+}
