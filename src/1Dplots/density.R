28f047a75a670357b5e884f0b45ee572c56e9042
adamwar7 <56248521+adamwar7@users.noreply.github.com>
2021-03-20 12:08:33 +0100

    Update density.R

--- src/1Dplots/density.R
+++ src/1Dplots/density.R
@@ -1,74 +1,13 @@
 makeDenisty <- function(foresty)
 {
  
-#Wykres gestosci dla dni tygodnia
-Poniedzialek <- 0 
-Wtorek <- 0 
-Sroda <- 0 
-Czwartek <- 0 
-Piatek <- 0 
-Sobota <- 0 
-Niedziela <- 0 
-for(i in foresty$day){
-  if(i == "mon"){
-    Poniedzialek = Poniedzialek + 1
-    }
-  else if(i == "tue"){
-    Wtorek = Wtorek + 1
-  }
-  else if(i == "wed"){
-    Sroda = Sroda  + 1
-  }
-  else if(i == "thu"){
-    Czwartek = Czwartek + 1
-  }
-  else if(i == "fri"){
-    Piatek = Piatek + 1
-  }
-  else if(i == "sat"){
-    Sobota = Sobota + 1
-  }
-  else if(i == "sun"){
-    Niedziela = Niedziela + 1
-  }
-}
-
-days <-c(Poniedzialek,Wtorek,Sroda,Czwartek,Piatek,Sobota,Niedziela)
-gestosc <- density(days)
+#Wykres gestosci dla dni
+gestosc <- density(foresty$day)
 plot(gestosc, frame = TRUE, col = "steelblue", main = "Gestosc pozarow dla dni tygodnia", sub="N = dni tygodnia")
 polygon(gestosc, col = "steelblue", bor = "black")
 
-
 #Wykres gestosci dla miesiecy
-jan =0; feb=0; mar=0; apr=0; may=0; jun=0; jul=0; aug=0; sep=0; oct=0; nov=0; dec=0;
-for(i in foresty$month){
-  if(i == "jan"){
-    jan = jan + 1
-  }else if(i=="feb"){
-    feb = feb + 1
-  }else if(i=="mar"){
-    mar = mar + 1
-  }else if(i == "apr"){
-    apr = apr + 1
-  }else if(i=="may"){
-    may = may + 1
-  }else if(i=="jun"){
-    jun = jun+1
-  }else if(i=="jul"){
-    jul = jul+1
-  }else if(i=="aug"){
-    aug = aug +1
-  }else if(i=="sep"){
-    sep = sep+1
-  }else if(i=="oct"){
-    oct = oct + 1
-  }else if(i=="nov"){
-    nov = nov +1
-  }else if(i=="dec"){
-    dec = dec + 1
-  }
-}
-dataMonths = c(jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
+gestosc <- density( foresty$month)
 plot(gestosc, frame = TRUE, col = "red", main = "Gestosc pozarow dla miesiecy", sub="N = miesiace")
 polygon(gestosc, col = "red")
 
