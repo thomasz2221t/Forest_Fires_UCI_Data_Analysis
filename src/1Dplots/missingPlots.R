f6666994790149c86dea22e7ea4280067e5061a8
AntoniLS <61624166+AntoniLS@users.noreply.github.com>
2021-03-24 10:05:03 +0100

    zapis wykresów do png

--- src/1Dplots/missingPlots.R
+++ src/1Dplots/missingPlots.R
@@ -9,7 +9,7 @@ forestyblad <-read.csv(file = 'data/forestfireskopia.csv')
 
 # Create separate boxplots for each attribute
 #Months and days
-png(filename= "plotsImages/myplot1.png")
+png(filename= "plotsImages/boxandwhisker1.png")
 par(mfrow=c(1,6),mfcol=c(1,2))
 for(i in 3:4) {
   boxplot(foresty[,i], main=names(foresty)[i])
@@ -17,21 +17,26 @@ for(i in 3:4) {
 dev.off()
 
 #FFMC, DMC, DC and ISI
+png(filename= "plotsImages/boxandwhisker2.png")
 par(mfrow=c(1,6))
 for(i in 5:8) {
   boxplot(foresty[,i], main=names(foresty)[i])
 }
+dev.off()
 
 #tem, RH, wind, rain and area
+png(filename= "plotsImages/boxandwhisker3.png")
 par(mfrow=c(1,6))
 for(i in 9:13) {
   boxplot(foresty[,i], main=names(foresty)[i])
 }
+dev.off()
 
-
+png(filename= "plotsImages/missingplots1.png") #dwa missing plots obok siebie
 #Missing plot incorrect values in csv file
 missmap(forestyblad, col=c("black", "grey"), legend=FALSE,main = "Brakujce wartosci (celowe bledy)")
 
 #Missing plot original csv file
 missmap(foresty, col=c("black", "grey"), legend=FALSE,main = "Brakujce wartosci")
 }
+dev.off()
