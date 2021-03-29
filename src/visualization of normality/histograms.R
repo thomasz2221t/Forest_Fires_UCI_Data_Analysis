7f8998d64436491fc53b0fa77c56a6697cfbfa97
Pczuczelo <56248545+Pczuczelo@users.noreply.github.com>
2021-03-29 19:29:52 +0200

    histogramy

--- src/visualization of normality/histograms.R
+++ src/visualization of normality/histograms.R
@@ -2,30 +2,39 @@ makeHistograms<-function(data)
 {
 
 Temperature <- data$temp
+png(filename= "plotsImages/histogram-1.png")
 hist(Temperature,
      ylim=c(0,200),
      xlab = "Temperature in Celsius degree",
      ylab="Number of apperances",
      col="red")
-
+dev.off()
 
 Wind <- data$wind
+
+png(filename= "plotsImages/histogram-2.png")
 hist(Wind,
      xlim=c(0,11),
      ylim=c(0,200),
      xlab = "Value of wind [m/s]",
      ylab="Number of apperances",
      col="green")
+dev.off()
 
 DMC <- data$DMC
+
+png(filename= "plotsImages/histogram-3.png")
 hist(DMC,
      xlim=c(0,320),
      ylim=c(0,100),
      xlab = "Code of litter humidity from FWI System",
      ylab="Number of apperances",
      col="orange")
+dev.off()
 
 RH <- data$RH
+
+png(filename= "plotsImages/histogram-4.png")
 hist(RH,
      main=paste("Histogram of relative humidity"),     
      xlim=c(0,100),
@@ -33,8 +42,11 @@ hist(RH,
      xlab = "Relative humidity [%]",
      ylab="Number of apperances",
      col="blue")
+dev.off()
 
 DC <- data$DC
+
+png(filename= "plotsImages/histogram-5.png")
 hist(DC,
      main=paste("Histogram of drought code index"),     
      xlim=c(0,1000),
@@ -42,4 +54,5 @@ hist(DC,
      xlab = "Drought code index from FWI System",
      ylab="Number of apperances",
      col="brown")
+dev.off()
 }
